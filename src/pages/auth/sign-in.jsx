124340b50bac64daf3a04e794d23a75d159c89ff
Anésio Neto <anesios98@gmail.com>
2024-03-23 19:27:31 -0300

    :construction: Auth page translated

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -1,8 +1,8 @@
 import {
-  Card,
-  Input,
-  Checkbox,
   Button,
+  Card,
+  Checkbox,
+  Input,
   Typography,
 } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
@@ -13,24 +13,24 @@ export function SignIn() {
     <section className="m-8 flex gap-4">
       <div className="w-full lg:w-3/5 mt-24">
         <div className="text-center">
-          <Typography variant="h2" className="font-bold mb-4">Sign In</Typography>
-          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Enter your email and password to Sign In.</Typography>
+          <Typography variant="h2" className="font-bold mb-4">Entrar</Typography>
+          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Digite seu e-mail e senha para entrar.</Typography>
         </div>
         <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
           <div className="mb-1 flex flex-col gap-6">
             <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
-              Your email
+              Email
             </Typography>
             <Input
               size="lg"
-              placeholder="name@mail.com"
+              placeholder="exemplo@gmail.com"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
             />
             <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
-              Password
+              Senha
             </Typography>
             <Input
               type="password"
@@ -42,26 +42,31 @@ export function SignIn() {
               }}
             />
           </div>
+          <Typography variant="small" className="flex justify-end font-medium text-gray-900 mt-2">
+              <a href="#">
+                Esqueci a senha
+              </a>
+            </Typography>
           <Checkbox
             label={
               <Typography
                 variant="small"
                 color="gray"
-                className="flex items-center justify-start font-medium"
+                className="mt-3 flex items-center justify-start font-medium"
               >
-                I agree the&nbsp;
+                Concordo com os &nbsp;
                 <a
                   href="#"
                   className="font-normal text-black transition-colors hover:text-gray-900 underline"
                 >
-                  Terms and Conditions
+                  Termos e condições
                 </a>
               </Typography>
             }
-            containerProps={{ className: "-ml-2.5" }}
+            containerProps={{ className: "-ml-2.5 mt-3" }}
           />
           <Button className="mt-6" fullWidth>
-            Sign In
+            Entrar
           </Button>
 
           <div className="flex items-center justify-between gap-2 mt-6">
@@ -72,16 +77,12 @@ export function SignIn() {
                   color="gray"
                   className="flex items-center justify-start font-medium"
                 >
-                  Subscribe me to newsletter
+                  Me inscreva na newsletter
                 </Typography>
               }
               containerProps={{ className: "-ml-2.5" }}
             />
-            <Typography variant="small" className="font-medium text-gray-900">
-              <a href="#">
-                Forgot Password
-              </a>
-            </Typography>
+
           </div>
           <div className="space-y-4 mt-8">
             <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
@@ -98,16 +99,16 @@ export function SignIn() {
                   </clipPath>
                 </defs>
               </svg>
-              <span>Sign in With Google</span>
+              <span>Faça login com o Google</span>
             </Button>
             <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
               <img src="/img/twitter-logo.svg" height={24} width={24} alt="" />
-              <span>Sign in With Twitter</span>
+              <span>Faça login com o Twitter</span>
             </Button>
           </div>
           <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
-            Not registered?
-            <Link to="/auth/sign-up" className="text-gray-900 ml-1">Create account</Link>
+            Não está registrado?
+            <Link to="/auth/sign-up" className="text-gray-900 ml-1 underline">Crie uma conta</Link>
           </Typography>
         </form>
 
