cb71470caee80d230b6c5bc18347cce915016df3
Anésio Neto <anesios98@gmail.com>
2024-03-24 20:12:51 -0300

    :start: Translated Navbar

--- src/routes.jsx
+++ src/routes.jsx
@@ -19,7 +19,7 @@ export const routes = [
     pages: [
       {
         icon: <HomeIcon {...icon} />,
-        name: "painel administrativo",
+        name: "dashboard",
         path: "/home",
         element: <Home />,
       },
