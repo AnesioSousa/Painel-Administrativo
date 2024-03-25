ff8e0874be5f3a3d13aabad8c0e76a9389e62ffe
Anésio Neto <anesios98@gmail.com>
2024-03-25 15:07:13 -0300

    :star: Changes on statistics card data

--- src/data/statistics-cards-data.js
+++ src/data/statistics-cards-data.js
@@ -1,53 +1,53 @@
 import {
   BanknotesIcon,
+  ChartBarIcon,
   UserPlusIcon,
   UsersIcon,
-  ChartBarIcon,
 } from "@heroicons/react/24/solid";
 
 export const statisticsCardsData = [
   {
     color: "gray",
     icon: BanknotesIcon,
-    title: "Today's Money",
-    value: "$53k",
+    title: "Renda Obtida Hoje",
+    value: "R$ 53k",
     footer: {
       color: "text-green-500",
       value: "+55%",
-      label: "than last week",
+      label: "de crescimento em relação a semana passada",
     },
   },
   {
     color: "gray",
     icon: UsersIcon,
-    title: "Today's Users",
-    value: "2,300",
+    title: "Novos Usuários Hoje",
+    value: "2.300",
     footer: {
       color: "text-green-500",
       value: "+3%",
-      label: "than last month",
+      label: "de crescimento em relação ao último mês",
     },
   },
   {
     color: "gray",
     icon: UserPlusIcon,
-    title: "New Clients",
-    value: "3,462",
+    title: "Novos Clientes",
+    value: "3.462",
     footer: {
       color: "text-red-500",
       value: "-2%",
-      label: "than yesterday",
+      label: "do que ontem",
     },
   },
   {
     color: "gray",
     icon: ChartBarIcon,
-    title: "Sales",
-    value: "$103,430",
+    title: "Vendas",
+    value: "R$ 103.430",
     footer: {
       color: "text-green-500",
       value: "+5%",
-      label: "than yesterday",
+      label: "do que ontem",
     },
   },
 ];
